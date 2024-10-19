da5eff53c7ebd7f79e482f1dc2ade02c087201dc
otabek-art <pocani497@gmail.com>
2024-10-19 17:03:03 +0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import './App.css'
-import Card from './components/Card/Card'
-import Card2 from './components/Card/Card2'
+import React from "react";
+import "./App.css";
+import Card from "./components/Card/Card";
+import Card3 from "./components/Card/Card3";
+
 
 const App = () => {
   return (
-  <>
-  <section>
-    <div className="container">
-      <div className="wrap">
+    <>
+      <section>
+        <div className="container">
+          <div className="wrap">
+            <Card />
+            <Card3/>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-   <Card/>
-   <Card2/>
-      </div>
-
-    </div>
-  </section>
-  </>
-  )
-}
-
-export default App
+export default App;
---
+++ src/components/Card/Card3.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Card3 = () => {
+  return (
+    <div className="card card2">
+      <img
+        src="https://dznike-githubuzer1237s-projects.vercel.app/nike-blue.png"
+        alt=""
+      />
+      <h2>Nike x off-white</h2>
+      <p> Lorem ipsum dolor sit amet.</p>
+      <h3>1599$</h3>
+
+      <div>
+        <button>Buy now</button>
+        <button>⭐</button>
+      </div>
+    </div>
+  );
+};
+
+export default Card3;
