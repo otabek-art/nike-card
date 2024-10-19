faefefed3e8ba68f02f722c146d23e78c7bc8fdb
otabek-art <pocani497@gmail.com>
2024-10-19 16:45:23 +0500

    Add files via upload

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import'./Card.css'
+
+const Card = () => {
+  return (
+<>
+<div className="card card1">
+        <img src="https://i.ibb.co/cNWqxGx/red.png" alt="" />
+        <h2>Nike x off-white</h2>
+        <p> Lorem ipsum dolor sit amet.</p>
+        <h3>999$</h3>
+
+        <div>
+          <button>Buy now</button>
+          <button>⭐</button>
+        </div>
+      </div>
+</>
+  )
+}
+
+export default Card
---
+++ src/components/Card/Card2.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Card2 = () => {
+  return (
+    <div className="card card2">
+    <img src="https://dznike-githubuzer1237s-projects.vercel.app/nike-blue.png" alt="" />
+    <h2>Nike x off-white</h2>
+    <p> Lorem ipsum dolor sit amet.</p>
+    <h3>1599$</h3>
+
+    <div>
+      <button>Buy now</button>
+      <button>⭐</button>
+    </div>
+  </div>
+  )
+}
+
+export default Card2
